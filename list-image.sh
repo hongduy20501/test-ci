076f958b027b93231a75e1b2b0ede461eb7218d7
hongduy20501 <hongduy205@gmail.com>
2023-08-18 07:39:46 +0000

    test

--- list-image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-for item in $(docker service ls -q --filter label=com.docker.stack.namespace=${1} --format '{{ .Name }}'); do
-  image=$(docker service inspect ${item} | jq -r .[].Spec.TaskTemplate.ContainerSpec.Image | sed 's/@.*$//')
-  echo ${item} ${image}
-done
